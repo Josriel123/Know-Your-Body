8ca67cf89127995a922efa92ff8eb870ae881dc5
Joel <joelbuenolopez@gmail.com>
2024-07-26 15:10:08 -0400

    u

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.getElementById('burgerMenu').addEventListener('click', function() {
     var navMenu = document.getElementById('navMenu');
     if (navMenu.style.left === '0px') {
-        navMenu.style.left = '-40%'; //Hide the menu
+        navMenu.style.left = '-35%'; //Hide the menu
     } else {
         navMenu.style.left = '0px'; // Showw the menu
     }
