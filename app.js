54e471917c0e9d82bbaa0f1fe2b2d58294b9f2ea
Joel <joelbuenolopez@gmail.com>
2024-07-30 01:00:39 -0400

    u

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 document.getElementById('startButton').addEventListener('click', function() {
+    // Hide all content except the general-section
+    document.getElementById('header').classList.add('hidden');
+    document.getElementById('sloganContainer').classList.add('hidden');
+    document.querySelector('.start').classList.add('hidden');
+    
     var generalSection = document.getElementById('generalSection');
-    generalSection.style.display = 'flex';
-    generalSection.scrollIntoView({ behavior: 'smooth' });
+    generalSection.style.display = 'flex'; // Show the general-section
+    generalSection.scrollIntoView({ behavior: 'smooth' }); // Scroll to the general-section
 });
