59c00a84d70a6d8b6dc5d9a34b786f8a356728b1
Joel <joelbuenolopez@gmail.com>
2024-07-30 21:14:19 -0400

    u

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ document.getElementById('startButton').addEventListener('click', function() {
     generalSection.innerHTML = `
         <a-scene>
             <a-assets>
-                <a-asset-item id="human-model" src="Human 3D Models/scene.gltf"></a-asset-item>
+                <a-asset-item id="humanModel" src="Human 3D Models/scene.gltf"></a-asset-item>
             </a-assets>
-            <a-camera position="0 1.6 2" orbit-controls="target: #human-model"></a-camera>
-            <a-entity id="human-model" gltf-model="#human-model" position="0 0 -5" rotation="0 45 0"></a-entity>
+            <a-camera position="0 1.6 3" orbit-controls="target: #humanModel"></a-camera>
+            <a-entity id="humanModel" gltf-model="#humanModel" position="0 0 -3" rotation="0 45 0" scale="1 1 1"></a-entity>
             <a-sky color="#ECECEC"></a-sky>
         </a-scene>
     `;
-    
+
     generalSection.scrollIntoView({ behavior: 'smooth' });
 });
